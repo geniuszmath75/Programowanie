db3305f9589834780d7c453c899ff1fa41a506eb
geniuszmath75 <geniuszmath75@gmail.com>
2021-11-14 12:35:11 +0100

    Zmodyfikowanie kodu(usunięcie goto)

--- C++/Strukturalne/liczby_pseudolosowe/gra_1000.cpp
+++ C++/Strukturalne/liczby_pseudolosowe/gra_1000.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cstdlib>
-#include <cstdio>
 #include <ctime>
 
 using namespace std;
@@ -8,26 +7,29 @@ using namespace std;
 int main()
 {
     srand( time(NULL));
-    int b = (std::rand() % 1000 ) + 1;
+    int b = (rand() % 100 ) + 1;
     int a;
-
-    wybor:
-    cout << "Podaj liczbe z zakresu 1-1000: ";
-    cin >> a;
-
-        if(a > b){
-            cout << "Podana liczba jest za duza.\nSprobuj jeszcze raz.\n";
-            goto wybor;
+    
+    do
+    {
+        cout << "Podaj liczbe z zakresu 1-100: ";
+        cin >> a;
+        if(a == b)
+        {
+            cout << "Prawidlowa liczba!";
         }
         else
-            if(a < b){
-                cout << "Podana liczba jest za mala.\nSprobuj jeszcze raz.\n";
-                goto wybor;
+        {
+            if(a > b)
+            {
+                cout << "Podana liczba jest za duza.\nSprobuj jeszcze raz.\n";
             }
             else
-                 if(a == b){
-                     cout << "Prawidlowa liczba!";
+            {
+                cout << "Podana liczba jest za mala.\nSprobuj jeszcze raz.\n";   
+            }
         }
+    }while(a != b);
 
     return 0;
 }
