0c7573f4e7cbd24516b16529895b40e607d53a6e
geniuszmath75 <geniuszmath75@gmail.com>
2021-11-23 19:36:58 +0100

    Utworzenie projektu

---
+++ C++/Strukturalne/Wariacja/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void wariacja(number)
+{
+    int cyfra1, cyfra2, cyfra3, cyfra4, number;
+
+    for(int i = 0; i < 5040; i++)
+    {
+
+    }
+}
+
+int main()
+{
+    srand( time(NULL) );
+    int liczba;
+    cout << "Podaj liczbe z przedzialu od 0 - 5039:";
+    cin >> liczba;
+    wariacja(liczba);
+
+    return 0;
+}
+
+
