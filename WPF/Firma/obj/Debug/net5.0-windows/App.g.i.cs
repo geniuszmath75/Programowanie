0a8f1c1b9dd5682fc673601a7df91fc7169f66be
geniuszmath75 <geniuszmath75@gmail.com>
2022-02-24 19:32:21 +0100

    Dodanie styli

--- WPF/Firma/obj/Debug/net5.0-windows/App.g.i.cs
+++ WPF/Firma/obj/Debug/net5.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Firma {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Firma {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
         public static void Main() {
             Firma.App app = new Firma.App();
             app.InitializeComponent();
