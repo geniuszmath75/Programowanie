4efbcf199d0d10a182ec73fb7822b9ab4f0916a4
geniuszmath75 <geniuszmath75@gmail.com>
2022-12-07 13:22:23 +0100

    Obsługa lokalizacji.

---
+++ Xamarin/Geolocation/Geolocation/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
+
+namespace Geolocation
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 50;
+            var position = await locator.GetPositionAsync
+            (TimeSpan.FromSeconds(100), null, true);
+            LongitudeLabel.Text = position.Longitude.ToString();
+            LatitudeLabel.Text = position.Latitude.ToString();
+        }
+
+    }
+}
