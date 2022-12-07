78de86ef5901f8e6c6c6a049742dae32732b65f0
geniuszmath75 <geniuszmath75@gmail.com>
2022-12-07 12:35:28 +0100

    Obsługa aparatu w Xamarin

---
+++ Xamarin/Aparat/Aparat/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+
+namespace Aparat
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnCam_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var photo = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions()
+                {
+                    DefaultCamera = Plugin.Media.Abstractions.CameraDevice.Rear,
+                    Directory = "Xamarin",
+                    SaveToAlbum = true
+                });
+                if (photo != null)
+                    imgCam.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message.ToString(), "Ok");
+            }
+        }
+
+    }
+}
