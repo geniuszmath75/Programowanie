214861dfdf270f8df2daa4c6d01a0f2d5f70469e
geniuszmath75 <geniuszmath75@gmail.com>
2022-12-08 19:14:53 +0100

    Dodanie kalkulatora do obliczania opłacalności wyboru pizzy.

---
+++ Xamarin/PizzaXam/Konsola/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Pizza
+{
+    internal class Pizza
+    {
+        static void Main(string[] args)
+        {
+            int diameterPizza1, diameterPizza2;
+            double pricePizza1, pricePizza2;
+            double areaPizza1, areaPizza2;
+            double howManyCm1, howManyCm2;
+
+            Console.WriteLine("Program do przeliczania wyboru pizzy");
+            Console.WriteLine();
+
+            Console.Write("Podaj średnicę pierwszej pizzy: ");
+            diameterPizza1 = int.Parse(Console.ReadLine());
+            Console.Write("Podaj cenę pierwszej pizzy: ");
+            pricePizza1 = double.Parse(Console.ReadLine());
+            Console.Write("Podaj średnicę drugiej pizzy: ");
+            diameterPizza2 = int.Parse(Console.ReadLine());
+            Console.Write("Podaj cenę drugiej pizzy: ");
+            pricePizza2 = double.Parse(Console.ReadLine());
+            
+            areaPizza1 = areaOfPizza(diameterPizza1);
+            areaPizza2 = areaOfPizza(diameterPizza2);
+
+            howManyCm1 = howManyCm(areaPizza1, pricePizza1);
+            howManyCm2 = howManyCm(areaPizza2, pricePizza2);
+
+            Console.Write("Za 1 zł otrzymasz " + howManyCm1 + "cm2\n");
+            Console.Write("Za 1 zł otrzymasz " + howManyCm2 + "cm2\n");
+
+            if (howManyCm1 > howManyCm2)
+                Console.WriteLine("Wybierz pizze nr 1");
+            else
+                Console.WriteLine("Wybierz pizze nr 2");
+
+        }
+
+        static double areaOfPizza(int diameterPizza)
+        {
+            double areaPizza = (Math.PI * Math.Pow(diameterPizza / 2, 2));
+            return areaPizza;
+        }
+
+        static double howManyCm(double areaPizza, double pricePizza)
+        {
+            double cm = areaPizza / pricePizza;
+            return Math.Round(cm, 2);
+        }
+    }
+}
---
+++ Xamarin/PizzaXam/PizzaXam/PizzaXam/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PizzaXam
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void oblicz_Clicked(object sender, EventArgs e)
+        {
+            int diameterPizza1 = int.Parse(diameter1.Text);
+            int diameterPizza2 = int.Parse(diameter2.Text);
+            double pricePizza1 = double.Parse(price1.Text);
+            double pricePizza2 = double.Parse(price2.Text);
+            double areaPizza1, areaPizza2;
+            double howManyCm1, howManyCm2;
+
+            areaPizza1 = areaOfPizza(diameterPizza1);
+            areaPizza2 = areaOfPizza(diameterPizza2);
+
+            howManyCm1 = howManyCm(areaPizza1, pricePizza1);
+            howManyCm2 = howManyCm(areaPizza2, pricePizza2);
+
+            wynik.Text += howManyCm1 + "cm2";
+            wynik2.Text += howManyCm2 + "cm2";
+
+            if (howManyCm1 > howManyCm2)
+                wynik3.Text = "Wybierz pizze nr 1";
+            else
+                wynik3.Text = "Wybierz pizze nr 2";
+        }
+
+        static double areaOfPizza(int diameterPizza)
+        {
+            double areaPizza = (Math.PI * Math.Pow(diameterPizza / 2, 2));
+            return areaPizza;
+        }
+
+        static double howManyCm(double areaPizza, double pricePizza)
+        {
+            double cm = areaPizza / pricePizza;
+            return Math.Round(cm, 2);
+        }
+
+        private void reset_Clicked(object sender, EventArgs e)
+        {
+            diameter1.Text = "";
+            diameter2.Text = "";
+            price1.Text = "";
+            price2.Text = "";
+            wynik.Text = "";
+            wynik2.Text = "";
+            wynik3.Text = "";
+        }
+    }
+}
