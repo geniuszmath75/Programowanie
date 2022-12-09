6ead6fa4f79e2b672e41b9d5547325605c06af7a
geniuszmath75 <geniuszmath75@gmail.com>
2022-12-09 08:40:28 +0100

    Obsługa notatek.

---
+++ Xamarin/Notes/Notes/Views/AboutPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Essentials;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Notes.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            await Launcher.OpenAsync("http://korepetycjenowysacz.edu.pl"); //otwiera link w przeglądarce
+        }
+    }
+}
---
+++ Xamarin/Notes/Notes/Views/NotesPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using Notes.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Notes.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NotesPage : ContentPage
+    {
+        public NotesPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var notes = new List<Note>();
+
+            //Create a Note object from each file
+            var files = Directory.EnumerateFiles(App.FolderPath, "*.notes.txt");
+            foreach(var filename in files)
+            {
+                notes.Add(new Note
+                {
+                    Filename = filename,
+                    Text = File.ReadAllText(filename),
+                    Date = File.GetCreationTime(filename)
+                });
+            }
+
+            //Set the data source for the CollectionView to a sorted collection of notes
+            collectionView.ItemsSource = notes.OrderBy(d => d.Date).ToList();
+        }
+        async void OnAddClicked(object sender, EventArgs e)
+        {
+            //Navigate to the NoteEntryPage, without passing any data
+            await Shell.Current.GoToAsync(nameof(NoteEntryPage));
+        }
+
+        async void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.CurrentSelection != null)
+            {
+                //Navigate to the NoteEntryPage, passing the filename as a query parameter
+
+                Note note = (Note)e.CurrentSelection.FirstOrDefault();
+                await Shell.Current.GoToAsync($"{nameof(NoteEntryPage)}?{nameof(NoteEntryPage.ItemId)}={note.Filename}");
+            }
+        }
+    }
+}
